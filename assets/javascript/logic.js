ef216bdb051cc964f0216811fc39affb16079cdf
chrisxjacobi <chrisxjacobi@gmail.com>
2016-08-28 22:05:08 -0500

    gifs appending plus buttons

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -24,4 +24,33 @@ $('#addGif').on('click', function() {
     renderButtons();
     return false;
 });
+
 renderButtons();
+
+$('button').on('click', function() {
+    var p = $(this).data('name');
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({ url: queryURL, method: 'GET' })
+        .done(function(response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $('<div class="item">');
+                var rating = results[i].rating;
+                var p = $('<p>').text("Rating: " + rating);
+                
+                var personImage = $('<img>');
+                personImage.attr('src', results[i].images.fixed_height.url);
+
+                gifDiv.append(p)
+                gifDiv.append(personImage)
+
+                $('#gifsAppearHere').prepend(gifDiv);
+            }
+        });
+
+});
+
+
+
